6be0e60e06c82181d9caa6b387554791180ab6ea
susanth18 <susanth18112k2@gmail.com>
2025-02-19 16:55:29 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from CDClassifier import logger
+from CDClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is a test message")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
